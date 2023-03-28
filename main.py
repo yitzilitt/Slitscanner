ac5a4d41cb3c6956cc9e498d4d1e98535b4d4a6b
yitzilitt <yitzilitt@gmail.com>
2023-03-28 13:55:51 -0400

    Modified main.py to be more user-friendly

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-# This takes every video file in a given directory, and applies the slitscan effect to them.
+# This takes every video file in a given directory and applies the slitscan effect to them.
 from SlitScanner_final import slit_scan_video
 import os
 
+# Accepts a user-input file path and normalizes it to work on both Mac and Windows systems
+directory = input("Paste the full path to a folder which contains the input video(s) which you wish to "
+                  "apply the effect to (e.g. 'C:\\Users\\Bob\\Desktop\\example folder' on Windows or "
+                  "'/home/Bob/Desktop/example folder' on Mac; also make sure it's only videos in the folder):" + "\n")
 
-directory = r'C:\Users\yitzi\Pictures\cloud slitscan stuff'
+# Convert user input path to a platform-independent format
+directory = os.path.normpath(directory)
 
 for filename in os.listdir(directory):
     if os.path.isfile(os.path.join(directory, filename)):
@@ -11,3 +16,4 @@ for filename in os.listdir(directory):
         output_filename = f"OUTPUT_{filename}"
         output_path = os.path.join(directory, output_filename)
         slit_scan_video(input_path, output_path)
+        print("Finished! Check your folder to see the output video(s).")
