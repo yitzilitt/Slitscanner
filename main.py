0e8d8cb619255501e66840bca57d11a6d1474e22
yitzilitt <yitzilitt@gmail.com>
2023-03-29 01:30:24 -0400

    Update main.py
    trying to do some error correction for mac/windows

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from SlitScanner_final import slit_scan_video
 import os
 
 # Accepts a user-input file path and normalizes it to work on both Mac and Windows systems
-directory = input("Paste the full path to a folder which contains the input video(s) which you wish to "
+
+def get_directory_input(prompt):
+    while True:
+        directory = input(prompt)
+        if directory.startswith(("'", '"')) and directory.endswith(("'", '"')):
+            return directory[1:-1]
+        elif " " in directory or "\\" in directory or "/" in directory:
+            return directory
+        else:
+            print("Please make sure to include the full path, with either forward slashes or backslashes.")
+
+
+directory = get_directory_input(
+    "Paste the full path to a folder which contains the input video(s) which you wish to "
                   "apply the effect to (e.g. 'C:\\Users\\Bob\\Desktop\\example folder' on Windows or "
-                  "'/home/Bob/Desktop/example folder' on Mac; also make sure it's only videos in the folder):" + "\n")
+                  "'/home/Bob/Desktop/example folder' on Mac). Make sure it's only videos in the folder, and to enclose the path you give in quotes, if you are running this via terminal on Mac or Linux (this shouldn't be necessary on Windows):" + "\n"
+)
 
 # Convert user input path to a platform-independent format
 directory = os.path.normpath(directory)
